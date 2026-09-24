925b3457199e474fbd77e147af7113fcc5615181
Bored008 <dahiyahimanshu80@gmail.com>
2026-09-24 18:45:50 +0530

    Add solutions for Pascal's Triangle II and rename Pascal's Triangle I and III

--- 6array/2Medium/PascalTriangle2.cpp
+++ 6array/2Medium/PascalTriangle2.cpp
@@ -1,10 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+vector<int> getRow(int rowIndex)
+{
+    vector<int> triangle;
+    triangle.push_back(1);
+    long long previous =1;
+    for(int i =1; i<=rowIndex; i++){
+        long long current = previous*(rowIndex-i+1)/i;
+        triangle.push_back(current);
+        previous = current;
+    }
+    return triangle;
+}
 
+int main()
+{
+    int rowIndex = 3;
+    vector<int> triangle = getRow(rowIndex);
+    for(auto x : triangle){
+        cout<<x<<" ";
+    }
 
-int main(){
-    vector<int> nums = {};
-    
     return 0;
 }
