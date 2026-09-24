e2a54d4952af9b9948b735263b81efaf65b2a024
Bored008 <dahiyahimanshu80@gmail.com>
2026-09-24 17:26:32 +0530

    Add solution for Pascal's Triangle (LeetCode 118)

--- 6array/2Medium/pascalTriangle1.cpp
+++ 6array/2Medium/pascalTriangle1.cpp
@@ -3,12 +3,32 @@ using namespace std;
 
 vector<vector<int>> generate(int numRows)
 {
-    
+    vector<vector<int>> triangle;
+    for (int i = 0; i < numRows; i++)
+    {
+        vector<int> row(i + 1, 1);
+        for (int j = 1; j < i; j++)
+        {
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+        }
+        triangle.push_back(row);
+    }
+    return triangle;
 }
 
 int main()
 {
-    vector<int> numRows = 5;
+    int numRows = 5;
+    vector<vector<int>> triangle = generate(numRows);
+
+    for (auto row : triangle)
+    {
+        for (auto num : row)
+        {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
